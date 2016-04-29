64b07120fb6ca87fde28b449bcb963a4ba0859d4
Sophiaduan <duan35@purdue.edu>
2016-04-29 16:08:00 -0400

    Combine node and import

--- combined.py
+++ combined.py
@@ -3,7 +3,6 @@ from xmlutils.xml2csv import xml2csv
 import pandas as pd
 import numpy as np
 import math
-import itertools import combinations
 
 import xml.etree.ElementTree as ET
 overdict = dict()
@@ -54,6 +53,30 @@ for childroot in root:
 		if(len(tempdict)  > 2):
 			overdict[countNumber] = tempdict
 			countNumber += 1
-csv = pd.DataFrame.from_dict(overdict,orient = 'index')
+full = pd.DataFrame.from_dict(overdict,orient = 'index')
 
+node2 = dict()
+for row in full.itertuples():
+    poster = row[3]
+    if poster not in node2:
+        node1 = dict()
+        node1['start'] = row[5]
+        node2[poster] = node1
+    else:
+        if node2[poster]['start'] > row[5]:
+            node2[poster]['start'] = row[5]
+
+    commenter = row[4]
+    comTime = row[6]
+
+    if commenter not in node2:
+            node1 = dict()
+            node1['start'] = comTime
+            node2[commenter] = node1
+    else:
+        if node2[commenter]['start'] > comTime:
+            node2[commenter]['start'] = comTime
+
+new_node = pd.DataFrame.from_dict(node2,orient = 'index')
+new_node.to_csv('node_2015_new2.csv')
 
